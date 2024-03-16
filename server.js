fb83fe087c691f3a7e47daf1f04bc2902c086704
priyanshu kumar mishra <mishrapriyanshu793@gmail.com>
2024-03-16 21:23:56 +0530

    restaurant menu

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
 
 const port = process.env.PORT || 5000;
 
 const fs = require("fs");
-
 app.use(
   cors({
     origin: "http://localhost:1234",
     methods: ["GET"],
   })
 );
+
+const dbConnect = () =>
+  mongoose
+    .connect(
+      "mongodb+srv://mishrapriyanshu793:v3ZvK45OTL7mKtsx@mycluster.7voxtp7.mongodb.net/ProductsDB"
+    )
+    .then(() => {
+      console.log("Mongodb Connected Successfully");
+    })
+    .catch(() => {
+      console.log("Error in Connecting to Mongodb Server");
+    });
+
+dbConnect();
 
 app.get("/api/restaurants", (req, res) => {
   fs.readFile("./mockData.json", "utf8", (err, data) => {
@@ -21,6 +35,22 @@ app.get("/api/restaurants", (req, res) => {
     }
     const response = JSON.parse(data);
     res.status(200).json(response);
+  });
+});
+app.get("/api/restaurants/:id", (req, res) => {
+  const id = req.params.id;
+
+  dbConnect().then(() => {
+    const db = mongoose.connection.db;
+    db.collection("products")
+      .find({ "data.cards.0.card.card.info.id": id })
+      .toArray()
+      .then((data) => {
+        res.json(data[0]);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   });
 });
 
